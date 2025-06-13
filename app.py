a316d9e21f111090bf7c379f331845fbf0953dfe
ME MOKH <mokhme12@gmail.com>
2025-06-14 00:33:53 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+import joblib
+
+# تحميل النموذج باسمك
+model = joblib.load("svm_fire_model_balanced.joblib")
+
+app = Flask(name)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    values = data["data"]  # بيانات الحساسات
+    prediction = int(model.predict([values])[0])
+    return jsonify({"prediction": prediction})
