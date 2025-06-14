ff168d2e8a177d3bb9e537d9580c2c79c78fa8a4
ME MOKH <mokhme12@gmail.com>
2025-06-14 16:41:27 +0200

    tru  typo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import numpy as np
 
 # تحميل النموذج
 model = joblib.load("svm_fire_model_balanced.joblib")
@@ -10,10 +11,15 @@ app = Flask(__name__)
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    values = data["data"]
-    prediction = int(model.predict([values])[0])
-    return jsonify({"prediction": prediction})
-
+    if not data or "data" not in data:
+        return jsonify({"error":"Missing 'data' field"}),400
+        try:
+            features=np.array(data["data"]).reshape(1, -1)
+            prediction=model.predict(features)[0]
+            return jsonify({"partition":int (prediction)})
+            except Exception as e:
+                return jsonify({"error":str(e)}),500
+ 
 # تشغيل التطبيق
 if __name__ == "__main__":
     app.run(host="0.0.0.0",
