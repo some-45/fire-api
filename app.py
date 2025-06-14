47ca3946a0ceb8a7497cb3935445926b35c35cab
ME MOKH <mokhme12@gmail.com>
2025-06-14 17:13:18 +0200

    lll

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ app = Flask(__name__)
 def predict():
     data = request.get_json()
     if not data or "data" not in data:
-        return jsonify({"error":"Missing 'data' field"}),400
+        return jsonify({"error": "Missing 'data' field"}), 400
+
     try:
-            features=np.array(data["data"]).reshape(1, -1)
-            prediction=model.predict(features)[0]
-            return jsonify({"partition":int (prediction)})
-   except Exception as e:
-                return jsonify({"error":str(e)}),500
- 
+        features = np.array(data["data"]).reshape(1, -1)
+        prediction = model.predict(features)[0]
+        return jsonify({"partition": int(prediction)})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 # تشغيل التطبيق
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",
-    port=10000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
