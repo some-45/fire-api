9a8bdcec0fd1ad451336baba4db2f42a28e9dfd0
ME MOKH <mokhme12@gmail.com>
2025-06-14 13:01:15 +0200

    Fix indentation error

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, jsonify
 import joblib
 
-# تحميل النموذج باسمك
+# تحميل النموذج
 model = joblib.load("svm_fire_model_balanced.joblib")
 
-app = Flask(__name__)
+# إنشاء التطبيق
+app = Flask(name)
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    values = data["data"]  # بيانات الحساسات
+    values = data["data"]
     prediction = int(model.predict([values])[0])
     return jsonify({"prediction": prediction})
-    if name == "main":
+
+# تشغيل التطبيق
+if name == "main":
     app.run(host="0.0.0.0",
     port=10000, debug=True)
